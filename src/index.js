d509ae8c74f12dc40e0cf4e83d606a3784a15273
k096906261.49@gmail.com <k096906261.49@gmail.com>
2019-09-23 17:35:34 +0900

    add routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App/App';
+import Main from './Dashboard/Main';
+import registerServiceWorker from './registerServiceWorker';
+import {Router, Route, browserHistory} from 'react-router';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Router history={browserHistory}>
+    <Route path="/" component={Main}/>
+    <Route path="/app" component={App}/>
+</Router>, document.getElementById('root'));
+registerServiceWorker();
